3b0e8f3d7788f42d0c9f0ce79e394efd6d805ab1
Zack  Arno <zackarno@gmail.com>
2023-11-17 15:54:50 -0500

    change era5 from daily to monthly

--- data-raw/era5_daily.R
+++ data-raw/era5_daily.R
@@ -9,7 +9,7 @@ fp_out <- file.path(
   "public",
   "processed",
   "lac",
-  "era5_raw_daily.csv"
+  "era5_raw_monthly.csv"
 )
 # Daily raw has data past 2020 and goes back to 1950
 # daily only has 1974-2020
@@ -22,7 +22,16 @@ date_df$time_start %>% range()
 era_filt <- era$filterDate("1981-01-01","2023-11-01")$select("total_precipitation_sum")
 
 scale_val <- 27830 
+era_tic <- as_tidyee(era_filt)
 
+era_tic_monthly <- era_tic %>% 
+  group_by(
+    year,month
+  ) %>% 
+    summarise(
+      stat= "sum"
+    )
+    
 
 cat("get adm0 boundaries from FAO\n")
 fc_adm0 = ee$FeatureCollection('FAO/GAUL/2015/level0')
@@ -34,7 +43,7 @@ fc_region_adm0 <- fc_adm0$filter(ee$Filter$inList("ADM0_NAME",  c("Guatemala",
 )
 
 cat("begin extraction of zonal stats\n")
-df_rainfall_adm0 <- ee_extract_tidy(x = era_filt,
+df_rainfall_adm0 <- ee_extract_tidy(x = era_tic_monthly,
                                     y = fc_region_adm0,
                                     scale = scale_val,
                                     stat = "mean",
