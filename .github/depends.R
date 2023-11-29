d399539a9fb1e02a9f0b9d1edc8457b38b62e119
Zack  Arno <zackarno@gmail.com>
2023-11-29 13:01:51 -0500

    testing workflow run for installing caching packages

--- .github/depends.R
+++ .github/depends.R
@@ -1,25 +1,24 @@
-# install.packages("remotes") # moving this to the top to see if it will cause the GH action to fail faster
 
-
-install.packages(c("dplyr",
-                   "ggplot2",
-                   "readr",
-                   "sf",
-                   # "rvest", # webscraping
-                   "janitor", # data cleaning - used in scraper
-                   "terra", # raster manipulation
-                   "exactextractr", # zonal stats
+install.packages(c("dplyr", #wrangling
+                   "ggplot2",# plotting
+                   "readr", #reading
+                   "purrr", # mapping
                    "tidyr", # pivot_wider
-                   # "zoo", # rolling stats
-                   "googledrive",
-                   "purrr",
                    "lubridate",
                    "stringr", # annoying dep just for str_trim to add a single "." to email.
+                   "zoo", # rolling stats
+                   "janitor", # data cleaning - maybe not used?
+                   "sf", # spatial 
+                   "terra", # raster manipulation
+                   "exactextractr", # zonal stats
+                   "googledrive",
                    "here", # set paths in rmds without thinking.
                    "blastula", # rmd emails
                    "glue",  # mainly for pasting text in email.
                    "remotes", # needed to install gghdx
                    "showtext", # needed to set fonts in gghdx
-                   "gghdx"
+                   "gghdx", # custom plotting
+                   "rnaturalearth", # used to create AOI bbox
+                   "ecmwfr"
                    ))
 
--- src/update_trigger.R
+++ src/update_trigger.R
@@ -18,25 +18,32 @@ library(ggplot2)
 library(tidyr) # pivot wider
 library(stringr)
 library(lubridate)
-# library(sf)
-# library(googlesheets4)
-library(janitor)
-# library(tmap)
-library(here)
-library(blastula)
+library(ecmwfr)
 library(googledrive)
-library(ggtext) # colored title
-library(glue)
 library(gghdx)
-library(rhdx)
+library(blastula)
+library(sf)
+library(terra)
+library(rnaturalearth)
+
+# 
+# # library(sf)
+# # library(googlesheets4)
+# library(janitor)
+# # library(tmap)
+# library(here)
+# library(ggtext) # colored title
+# library(glue)
+# library(rhdx)
 gghdx()
 
+ecmwf_leadimes <- c(1:6)
+run_date <- Sys.Date()
+# pub dates - 5th day of every month
+#https://www.ecmwf.int/en/newsletter/154/meteorology/ecmwfs-new-long-range-forecasting-system-seas5
 
-
-source(file.path("R","email_funcs.R"))
-source(file.path("src","email","email_utils.R"))
-
-
+# source(file.path("R","email_funcs.R"))
+# source(file.path("src","email","email_utils.R"))
 
 
 # Google Drive ------------------------------------------------------------
@@ -94,7 +101,67 @@ df_thresholds <- read_csv(dl_log_path)
 # hybrid notebook or if we sho
 
 
+#' Download ECMWF SEAS51 Data for the AOI (Nicaragua, Honduras, Guatemala, El Salvador)
+#' Uses [ecmwfr package](https://github.com/bluegreen-labs/ecmwfr) package to access [ECMWF Climate Data Store API ](https://cds.climate.copernicus.eu/)
+#' Registration instructions are available in teh above ecmwfr package link.
 
 
 
+
+# think only need to do this 1x
+ecmwfr::wf_set_key(
+  user=Sys.getenv("ECMWF_USER_UID"),
+  key = Sys.getenv("ECMWF_USER_KEY"),
+  service = "cds"
+)
+
+
+# Check Metadata/attributes for appropriate selection ---------------------
+
+## List data sets available
+df_cds_datasets<- wf_datasets(
+  user=Sys.getenv("ECMWF_USER_UID"),
+  service = "cds"
+)
+
+# get metdata of that specific data set ("seasonal-monthly-single-levels")
+prod_info <- wf_product_info(
+  dataset = "seasonal-monthly-single-levels",
+  user=Sys.getenv("ECMWF_USER_UID"),
+  service = "cds"
+)
+
+
+cat("defining bbox for extraction\n")
+aoi_countries <- ?ne_countries(country = c("Nicaragua","Honduras","Guatemala","El Salvador")) %>% 
+  st_as_sf() %>% 
+  select(
+    contains("admin"),
+    iso_a3
+  )
+aoi_bbox <- st_bbox(aoi_countries) 
+
+
+# Create API requests ---------------------------------------------------------
+pub_mo_date <- format(floor_date(run_date,"month"),"%Y%m%d")
+
+cat("writing data requests to list\n")
+request_coords<- glue("{aoi_bbox['ymin']}/{aoi_bbox['xmin']}/{aoi_bbox['ymax']}/{aoi_bbox['xmax']}")
+
+ecmwf_data_request <- ecmwf_leadimes %>% 
+  map(
+    ~list(
+      product_type = "monthly_mean",
+      format = "grib",
+      originating_centre = "ecmwf",
+      system="51",
+      variable = c("total_precipitation"),
+      year = as.character(year(run_date)),
+      month = sprintf("%02d",month(run_date)),
+      area = request_coords,
+      leadtime_month = .x,
+      dataset_short_name = "seasonal-monthly-single-levels",
+      target = glue("ecmwf_seas51_monthly_{pub_mo_date}_lt{.x}.grib")
+    )
+  )
 
