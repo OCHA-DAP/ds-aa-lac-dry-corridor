caae36357e01901cbb57cb8eea75b824a080082d
zackarno <zackarno@gmail.com>
2024-03-19 16:56:48 -0400

    add ecmwf-insuvimeh thresholds table

---
+++ R/tar_forecast_summaries.R
@@ -0,0 +1,96 @@
+
+#' ecmwf_summarise_seasons
+#' @details
+#' Works for default windows of "primera" and "postera" and Copernicus Data Store Public SEAS51 data
+#' **Note** when we get access to HRES, we will need to adjust line that creates `monitoring_mo_seq` as will have an extra
+#' month of lead time data
+#' **Note** -- sequencing of months would be disrupted/faulty if we start using months that extend into next year i.e (11,12,1,2,)
+#' 
+#' @param df data.frame with all ECMWF data aggregated to monthly precip values for each leadtime
+#' @param window_list list containing numeric vector representing months
+
+summarise_seasons <-  function(df=df_ecmwf_zonal_all,
+                                    window_list= list("primera"=c(5,6,7,8),
+                                                      "postera"=c(9,10,11)),
+                                    forecast_source ="mars"
+                                                      ){
+  df <- df %>% 
+    mutate(
+      valid_mo = month(valid_date)
+    ) %>% 
+    filter(
+      # ECMWF data was also reduced to admin boundaries w/ median stat, let's remove it for now.
+      stat== "mean"
+    )
+  # MARS & INSUVIMEH last forecast is publication date + 6 months
+  if(forecast_source %in% c("mars","insuvimeh")){
+    max_lt <- 6  
+  }
+  # CDs is publication date + 5 months
+  if(forecast_source=="cds"){
+    max_lt <- 5
+  }
+  
+  window_list %>% 
+    imap(\(valid_mo_seq, window_name){
+      
+      monitoring_mo_seq <- (max(valid_mo_seq)-max_lt ): min(valid_mo_seq)
+      df %>% 
+        filter(
+          valid_mo %in% valid_mo_seq,
+          month(pub_date) %in% monitoring_mo_seq
+        ) %>%
+        arrange(
+          adm0_es, pub_date, lt
+        ) %>% 
+        group_by(pub_date,adm0_es) %>% 
+        mutate(
+          count =length(unique(lt))
+        ) %>% 
+           summarise(
+             mm = sum(mm),
+             # **min() - BECAUSE**  for MJJA (5,6,7,8) at each pub_date we have a set of leadtimes
+             # for EXAMPLE in March we have the following leadtimes 2 
+                # 2 : March + 2 = May,
+                # 3 : March + 3 = June,
+                # 4 : March + 4 = July
+                # 5:  March + 5 = Aug
+             # Therefore when we sum those leadtime precip values we take min() of lt integer so we get the leadtime to first month being aggregated
+             lt= min(lt),
+             .groups = "drop"
+           ) %>% 
+           mutate(
+             window = window_name
+           )
+         }
+         
+    ) %>% 
+    list_rbind()
+
+}
+
+#' Title
+#' @description
+#' could actually be a potentially handy genreal function if I deal w/ 
+#' annoying lazy eval required by `group_by()`
+#' @return
+#' @export
+#'
+#' @examples
+grouped_quantile_summary <-  function(df,
+                                      x="mm",
+                                      grp_vars=c("adm0_es","window", "lt"),
+                                      rps=c(1:10)){
+   df %>% 
+    group_by(
+      !!!syms(grp_vars)
+    ) %>% 
+    reframe (
+      rp = rps,
+      q = 1/rp,
+      q_val = quantile(!!sym(x), probs =1/rp),
+      
+    ) 
+}
+
+
--- _targets.R
+++ _targets.R
@@ -1,39 +1,41 @@
-# Created by use_targets().
-# Follow the comments below to fill in this target script.
-# Then follow the manual to check and run the pipeline:
-#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline
+# Description -------------------------------------------------------------
+#' **Title**  Central American Dry Corridor (CADC) Trigger Inputs
+#' **Overview** Simplified pipeline to generate thresholds using forecast data available from
+#'  1. ECMWF MARS SEAS51 (since 1981)
+#'  2. INSUVIMEH  (since 1981)
+#' Exploratory analysis will be added back in a modular fashion. Removed from this version to facilitate review
+#' **Details** 
+#' The analysis is performed over the 4 countries through which the CADC runs (Nicaragua, Guatemala, Honduras, El Salvador).
+
 
 # Load packages required to define the pipeline:
 library(targets)
-library(rnaturalearth)
-library(sf)
-library(tidyverse)
-library(rgee)
-library(janitor)
-library(rgee)
-library(tidyrgee)
-library(exactextractr)
-library(terra)
-library(tidync)
-
-# library(tarchetypes) # Load other packages as needed.
-
-# Set target options:
-tar_option_set(
-  packages = c("tibble") # packages that your targets need to run
-)
-options(clustermq.scheduler = "multicore")
-
+library(tarchetypes) # dynanic file branching?
 tar_source()
 
-fp_iri_prob <- file.path(
-  Sys.getenv("AA_DATA_DIR"),
-  "private",
-  "processed",
-  "lac",
-  "iri",
-  "lac_iri_forecast_seasonal_precipitation_tercile_prob_Np18Sp10Em83Wm93.nc"
+# packages that your targets need to run
+tar_option_set(
+  packages = c("tidyverse",
+               "exactextractr",
+               "terra",
+               "tidync",
+               "sf",
+               "glue",
+               "tidyrgee",
+               "tidync",
+               "gt",
+               "rnaturalearth",
+               "sf",
+               "rgee",
+               "janitor"
+               # "girafe"
+  ) 
 )
+
+options(clustermq.scheduler = "multicore")
+
+# Define simple inputs -----------------------------------------------------------
+
 gdb_ecmwf_mars_tifs <- file.path(
   Sys.getenv("AA_DATA_DIR"),
   "private",
@@ -45,8 +47,17 @@ gdb_ecmwf_mars_tifs <- file.path(
 )
 
 
+gdb_insuvimeh_gtm <- file.path(
+  Sys.getenv("AA_DATA_DIR"),
+  "private",
+  "raw",
+  "lac",
+  "INSUVIMEH",
+  "Pronósticos_Precip_NextGen_Guatemala"
+)
 
 list(
+  ## Load admin boundaries ####
   tar_target(
     name = gdf_aoi_adm,
     command = read_rds(
@@ -59,116 +70,122 @@ list(
       )
     )
   ),
+  # ECMWF MARS --------------------------------------------------------------
+  
   tar_target(
-    name = df_iri_adm0,
-    command = iri_nc_to_r(
-      tnc_object = tidync(fp_iri_prob),
-      type = "prob"
-    ) %>%
-      imap_dfr(
-        \(rtmp, nm){
-          # loop through each leadtime - grab prob bavg and run zonal stats
-          # scary things happen if you don't deepcopy
-          r_bavg <- deepcopy(rtmp[[1]]) # bavg
-          exact_extract(
-            x = r_bavg,
-            y = gdf_aoi_adm$adm0,
-            fun = c("mean", "median"),
-            append_cols = c("adm0_es", "adm0_pcode")
-          ) %>%
-            pivot_longer(-matches("adm0_")) %>%
-            separate(name, into = c("stat", "date"), sep = "\\.") %>%
-            pivot_wider(names_from = "stat", values_from = "value") %>%
-            # add season information by combing dates w/ leadtimes
-            mutate(
-              leadtime = parse_number(nm),
-              predict_start_mo = as_date(date) + months(leadtime),
-              predict_mo1_abbr = str_sub(month(predict_start_mo, abbr = T, label = T), start = 1, end = 1),
-              predict_mo2_abbr = str_sub(month(predict_start_mo + months(1), abbr = T, label = T), start = 1, end = 1),
-              predict_mo3_abbr = str_sub(month(predict_start_mo + months(2), abbr = T, label = T), start = 1, end = 1),
-              end_yr_abbr = format(predict_start_mo + months(2), "%y")
-            ) %>%
-            unite(
-              "seas", predict_mo1_abbr:end_yr_abbr,
-              sep = ""
-            )
-        }
-      )
+    description = "PackedSpatRaster object containing each publication month leadtime combination as and individual band." ,
+    name = r_ecmwf_mars,
+    command = load_mars_raster(gdb = gdb_ecmwf_mars_tifs),
+    
   ),
   tar_target(
-    name = df_aoi_pixel_values,
-    command = iri_nc_to_r(
-      tnc_object = tidync(fp_iri_prob),
-      type = "prob"
-    ) %>%
-      imap_dfr(\(rt, nm){
-        # [[1]] first element is bavg (c1)
-        rt_copy <- deepcopy(rt[[1]])
-
-        # clip to aoi (instead of just bbox)
-        rt_clipped <- mask(
-          rt_copy,
-          # dissolve adm0 to 1 poly
-          gdf_aoi_adm$adm0 %>%
-            summarise()
-        )
-        rt_clipped %>%
-          terra::values() %>%
-          data.frame() %>%
-          pivot_longer(everything()) %>%
-          mutate(
-            leadtime = str_replace(nm, "lt", "leadtime "),
-            prob_cat = case_when(
-              value < 40 ~ "lt 40",
-              value < 50 ~ "lt 50",
-              value >= 50 ~ "gte 50"
-            )
-          )
-      })
+    description = "data.frame in long format containing zonal means for each publication month-leadtime combination",
+    name = df_ecmwf_mars,
+    command = zonal_ecmwf_mars(r_wrapped = r_ecmwf_mars, zone = gdf_aoi_adm$adm0, stat = "mean")
   ),
   tar_target(
-    name = df_cropland_lte_vhi_threshold,
-    command = cropland_lte_vhi_thresh(
-
-      # all monthly VHI raster directory
-      vhi_raster_dir = file.path(
-        Sys.getenv("AA_DATA_DIR"),
-        "private",
-        "raw",
-        "lac",
-        "vhi_fao"
-      ),
-      # file path to cropland fraction raster
-      cropland_raster_fp = file.path(
-        Sys.getenv("AA_DATA_DIR"),
-        "public",
-        "raw",
-        "glb",
-        "cropland",
-        "GlcShare_v10_02",
-        "glc_shv10_02.Tif"
-      ),
-
-      # admin 0 polygon
-      poly = gdf_aoi_adm$adm0,
-
-      # retain these cols in zonal stats
-      poly_cols = c(
-        "adm0_es",
-        "adm0_pcode"
-      ),
-      simplify_poly = 0.01,
-
-      # All VHI thresholds to run
-      threshold_seq = seq(0.05, 1, by = 0.05)
+    description = "data.frame where monthly rainfall values have been summed to seasons of interest for trigger (MJJA,SON)",
+    name = df_mars_seasonal_summarised,
+    command = summarise_seasons(df= df_ecmwf_mars %>% 
+                                  rename(mm="value") , # get in same format as other
+                                window_list= list("primera"=c(5,6,7,8),
+                                                  "postera"=c(9,10,11)),
+                                forecast_source = "mars"
     )
   ),
   tar_target(
-    name = r_ecmwf_mars,
-    command = load_mars_raster(gdb = gdb_ecmwf_mars_tifs)
+    description= "data.frame (long format) where quantiles defined by RPs 1-10 re provided for each season, country, leadtime",
+    name = df_mars_q_summary,
+    command = grouped_quantile_summary(df= df_mars_seasonal_summarised ,
+                                       x = "mm",
+                                       rps = c(1:10),
+                                       grp_vars=c("adm0_es","window", "lt")
+    )
   ),
+  # INSUVIMEH
   tar_target(
-    name = df_ecmwf_mars,
-    command = zonal_ecmwf_mars(r_wrapped = r_ecmwf_mars, zone = gdf_aoi_adm$adm0, stat = "mean")
+    description= "Catalogue of all data received from INSUVIMEH. Useful for troubleshooting at earlier stages when gaps.Gaps have been filled, but still might be useful in future",
+    name = df_gtm_nextgen_catalogue,
+    command = catalogue_insuvimeh_files(gdb = gdb_insuvimeh_gtm),
+  ),
+  
+  # load raster
+  tar_target(
+    description = "PackedSpatRaster object containing INSUVIMEH forecast with each publication month leadtime combination as and individual band." ,
+    name = r_wrap_gtm_nextgen,
+    command = load_insuvimeh_raster(gdb = gdb_insuvimeh_gtm) 
+    
+  ),
+  
+  # run zonal stats on insuvimeh
+  tar_target(
+    description = "INSUVIMEH monthly zonal means by leadtime",
+    name = df_gtm_nextgen_adm0,
+    command= zonal_gtm_insuvimeh(r = unwrap(r_wrap_gtm_nextgen),
+                               gdf = gdf_aoi_adm,
+                               rm_dup_years = F)
+  ),
+  # summarise by window, leadtime & pub date
+  tar_target(
+    description = "INSUVIMEH seasonal forecast sums by leadtime/window",
+    name = df_insuvimeh_seasonal_summarised,
+    command = summarise_seasons(df= df_gtm_nextgen_adm0 %>% 
+                                  rename(mm="value") , # get in same format as other
+                                window_list= list("primera"=c(5,6,7,8),
+                                                  "postera"=c(9,10,11)),
+                                forecast_source = "insuvimeh"
+    )
+  ),
+  
+  # final decision is to use ECMWF for final month of monitoring to
+  # include May forecast in May publication and to not include Sep monitoring of Postrera
+  tar_target(
+    description = "INSUVIMEH seasonal summaries with May removed from Primera monitoring",
+    name = df_insuvimeh_seasonal_summarised_filtered,
+    command =  df_insuvimeh_seasonal_summarised %>% 
+      # these filters are only necessary in INSUVIMEH data  since 
+      # can't include first month of valid month as publication month before running quantiles
+      # or it will be too low (i.e quantiles for primera will include only month 6,7,8)
+      filter(
+        !(month(pub_date)==5 & window=="primera"),
+        !(month(pub_date)==9 & window=="postera"),
+      ) %>%
+      # used through 2022 for ECMWF - so let's keep it standardized also let's not make threshold
+      # harder to hit because of drought that is still largely in effect (2023)
+      filter(year(pub_date)<2023)
+  ),
+  
+  # these will be used for to threshold Guatemala.
+  tar_target(
+    description = "INSUVIMEH historical record broken into quantile classes by window/lt",
+    name = df_insuvimeh_q_summary,
+    command = grouped_quantile_summary(df= df_insuvimeh_seasonal_summarised_filtered,
+                                       x = "mm",
+                                       rps = c(1:10),
+                                       grp_vars=c("adm0_es","window", "lt")
+    )
+  ),
+  
+  # final decision was to go w/ RP 4 so saving this for easy manipulation in 
+  # analysis/threshold_tables.qmd
+  tar_target(
+    description = "ECMWF & INSUVIMEH Thresholds in long format",
+    name = df_all_thresholds_rp4,
+    command = bind_rows(
+      df_insuvimeh_q_summary %>% 
+      filter(rp==4) %>% 
+      mutate(
+        forecast_source = "INSUVIMEH"
+      ),
+      df_mars_q_summary %>% 
+        filter(rp ==4) %>% 
+        mutate(
+          forecast_source = "ECMWF MARS"
+        )
   )
-)
+  )
+  # will add exploratory modules here.
+  )
+
+
+
