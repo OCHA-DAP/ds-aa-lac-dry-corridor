37b239e604693bfca3a526bfe8d295984751554e
zackarno <zackarno@gmail.com>
2024-03-22 12:49:22 -0400

    run styler

--- R/tar_insuvimeh.R
+++ R/tar_insuvimeh.R
@@ -6,97 +6,96 @@ library(sf)
 #' @description
 #' Provide the folder directory path to the insuvimeh files and return data.frame containing meta
 #' data to each file based on `n` file attributes and file/folder name structure
-#' 
+#'
 #' **note** non-generic function specific to this pipeline and the way we are currently receiving files from INSIVUMEH
 #'
 #' @param gdb `character` file path to parent folder containing all INSIVUMEH forecasts of interest.
 #' @param file_name_pattern `character` regex to correctly identify all files to be considered (default ='\\d{4}.nc$')
 #' @return data.frame with containing metadata on all files based on `nc` file attributes
 #' @examples \dontrun{
+#'
 #' }
-catalogue_insuvimeh_files <-  function(gdb,file_name_pattern =  "\\d{4}.nc$"){
-  
+catalogue_insuvimeh_files <- function(gdb, file_name_pattern = "\\d{4}.nc$") {
   fps <- list.files(gdb,
-                    full.names =T,
-                    recursive = T,
-                    pattern = file_name_pattern)
-  
+    full.names = T,
+    recursive = T,
+    pattern = file_name_pattern
+  )
+
   fps_w_parents <- list.files(gdb,
-                              recursive = T,
-                              pattern =file_name_pattern)
-  lr <- fps %>% 
-    map2(fps_w_parents,\(fp_nc,fp_par){
-      
+    recursive = T,
+    pattern = file_name_pattern
+  )
+  lr <- fps %>%
+    map2(fps_w_parents, \(fp_nc, fp_par){
       fp_base <- basename(fp_nc)
       # meta data collection  - to write band_name as "{pub_date}.lt_{leadtime}"
-      valid_time_meta <- ncmeta::nc_att(fp_nc,"T","units")$value$units
-      start_time_meta <- ncmeta::nc_att(fp_nc,"S","units")$value$units
-      valid_date <- as_date(str_extract(valid_time_meta,"\\d{4}-\\d{2}-\\d{2}"))
-      start_date <- as_date(str_extract(start_time_meta,"\\d{4}-\\d{2}-\\d{2}"))
-      lead_time_diff <- interval(start_date,valid_date)
+      valid_time_meta <- ncmeta::nc_att(fp_nc, "T", "units")$value$units
+      start_time_meta <- ncmeta::nc_att(fp_nc, "S", "units")$value$units
+      valid_date <- as_date(str_extract(valid_time_meta, "\\d{4}-\\d{2}-\\d{2}"))
+      start_date <- as_date(str_extract(start_time_meta, "\\d{4}-\\d{2}-\\d{2}"))
+      lead_time_diff <- interval(start_date, valid_date)
       lt <- lead_time_diff %/% months(1)
       tibble(
         file_name_parent = fp_par,
-        file_name= fp_base,
+        file_name = fp_base,
         pub_date = start_date,
-        valid_date =valid_date,
+        valid_date = valid_date,
         lt = lt
       )
-    }) %>% 
+    }) %>%
     list_rbind()
   return(lr)
 }
 
 
-load_insuvimeh_raster <-  function(gdb){
+load_insuvimeh_raster <- function(gdb) {
   fps <- list.files(gdb,
-                    full.names =T,
-                    recursive = T,
-                    pattern = "\\d{4}.nc$")
-  
-  lr <- fps %>% 
+    full.names = T,
+    recursive = T,
+    pattern = "\\d{4}.nc$"
+  )
+
+  lr <- fps %>%
     map(\(fp_nc){
-      
       # meta data collection  - to write band_name as "{pub_date}.lt_{leadtime}"
-      valid_time_meta <- ncmeta::nc_att(fp_nc,"T","units")$value$units
-      start_time_meta <- ncmeta::nc_att(fp_nc,"S","units")$value$units
-      valid_date <- as_date(str_extract(valid_time_meta,"\\d{4}-\\d{2}-\\d{2}"))
-      start_date <- as_date(str_extract(start_time_meta,"\\d{4}-\\d{2}-\\d{2}"))
-      lead_time_diff <- interval(start_date,valid_date)
+      valid_time_meta <- ncmeta::nc_att(fp_nc, "T", "units")$value$units
+      start_time_meta <- ncmeta::nc_att(fp_nc, "S", "units")$value$units
+      valid_date <- as_date(str_extract(valid_time_meta, "\\d{4}-\\d{2}-\\d{2}"))
+      start_date <- as_date(str_extract(start_time_meta, "\\d{4}-\\d{2}-\\d{2}"))
+      lead_time_diff <- interval(start_date, valid_date)
       lt <- lead_time_diff %/% months(1)
-      bname<- paste0(start_date,".lt_",lt)
-      
-      # Now we open the each file and turn it into a terra::rast() 
-      cat(bname,"\n")
+      bname <- paste0(start_date, ".lt_", lt)
+
+      # Now we open the each file and turn it into a terra::rast()
+      cat(bname, "\n")
       dat <- ncdf4::nc_open(fp_nc)
-      lon <- ncdf4::ncvar_get(dat,"X")
-      lat <- ncdf4::ncvar_get(dat,"Y")
-      value_array <- ncdf4::ncvar_get(dat,"deterministic")
-      value_array_fixed <- aperm(value_array,c(2,1))
-      
+      lon <- ncdf4::ncvar_get(dat, "X")
+      lat <- ncdf4::ncvar_get(dat, "Y")
+      value_array <- ncdf4::ncvar_get(dat, "deterministic")
+      value_array_fixed <- aperm(value_array, c(2, 1))
+
       rtmp <- terra::rast(
-        x= value_array_fixed,
-        extent =ext(
+        x = value_array_fixed,
+        extent = ext(
           min(lon),
           max(lon),
           min(lat),
           max(lat)
         ),
-        crs= "OGC:CRS84"
+        crs = "OGC:CRS84"
       )
       # set correct band nanme
-      rtmp %>% 
+      rtmp %>%
         set.names(bname)
       return(rtmp)
-    }
-    )
+    })
   # merge multi-band
-  r <- rast(lr ) 
-  
+  r <- rast(lr)
+
   # wrap so it can be saved as a target.
   r_wrap <- wrap(r)
   return(r_wrap)
-  
 }
 
 
@@ -112,30 +111,31 @@ load_insuvimeh_raster <-  function(gdb){
 #' @param rm_dup_years `logical` just parameter used in beginning when catalogue had issues
 #'
 #' @return `data.frame` in long format w/ zonal means by publication date and leadtime
-zonal_gtm_insuvimeh <-  function(r, gdf,rm_dup_years=F){
+zonal_gtm_insuvimeh <- function(r, gdf, rm_dup_years = F) {
   r_gtm <- unwrap(r)
-  if(rm_dup_years){
-    r_nm_yr_gtm <- as_date(str_extract(names(r_gtm),"\\d{4}-\\d{2}-\\d{2}") )
-    r_gtm <- r_gtm[[year(r_nm_yr_gtm)>1982]] # these years have duplicate folders  
+  if (rm_dup_years) {
+    r_nm_yr_gtm <- as_date(str_extract(names(r_gtm), "\\d{4}-\\d{2}-\\d{2}"))
+    r_gtm <- r_gtm[[year(r_nm_yr_gtm) > 1982]] # these years have duplicate folders
   }
-  
-  gdf_gtm_adm <- gdf %>% 
+
+  gdf_gtm_adm <- gdf %>%
     map(
-      ~.x %>% 
+      ~ .x %>%
         filter(
-          adm0_es=="Guatemala"
+          adm0_es == "Guatemala"
         )
     )
-  exact_extract(x = r_gtm,
-                y=gdf_gtm_adm$adm0,
-                fun = "mean")%>% 
-    pivot_longer(everything()) %>% 
-    separate(name,into = c("stat","pub_date","lt_chr"),sep = "\\.") %>% 
+  exact_extract(
+    x = r_gtm,
+    y = gdf_gtm_adm$adm0,
+    fun = "mean"
+  ) %>%
+    pivot_longer(everything()) %>%
+    separate(name, into = c("stat", "pub_date", "lt_chr"), sep = "\\.") %>%
     mutate(
-      adm0_es ="Guatemala",
-      pub_date= as_date(pub_date),
+      adm0_es = "Guatemala",
+      pub_date = as_date(pub_date),
       lt = parse_number(lt_chr),
       valid_date = pub_date + months(lt)
     )
-  
 }
--- _targets.R
+++ _targets.R
@@ -4,7 +4,7 @@
 #'  1. ECMWF MARS SEAS51 (since 1981)
 #'  2. INSUVIMEH  (since 1981)
 #' Exploratory analysis will be added back in a modular fashion. Removed from this version to facilitate review
-#' **Details** 
+#' **Details**
 #' The analysis is performed over the 4 countries through which the CADC runs (Nicaragua, Guatemala, Honduras, El Salvador).
 
 
@@ -15,11 +15,12 @@ tar_source()
 
 # packages that your targets need to run
 tar_option_set(
-  packages = c("tidyverse",
-               "exactextractr",
-               "terra",
-               "sf"
-  ) 
+  packages = c(
+    "tidyverse",
+    "exactextractr",
+    "terra",
+    "sf"
+  )
 )
 
 options(clustermq.scheduler = "multicore")
@@ -61,12 +62,11 @@ list(
     )
   ),
   # ECMWF MARS --------------------------------------------------------------
-  
+
   tar_target(
-    description = "PackedSpatRaster object containing each publication month leadtime combination as and individual band." ,
+    description = "PackedSpatRaster object containing each publication month leadtime combination as and individual band.",
     name = r_ecmwf_mars,
     command = load_mars_raster(gdb = gdb_ecmwf_mars_tifs),
-    
   ),
   tar_target(
     description = "data.frame in long format containing zonal means for each publication month-leadtime combination",
@@ -76,100 +76,106 @@ list(
   tar_target(
     description = "data.frame where monthly rainfall values have been summed to seasons of interest for trigger (MJJA,SON)",
     name = df_mars_seasonal_summarised,
-    command = summarise_seasons(df= df_ecmwf_mars %>% 
-                                  rename(mm="value") , # get in same format as other
-                                window_list= list("primera"=c(5,6,7,8),
-                                                  "postera"=c(9,10,11))
-                                
+    command = summarise_seasons(
+      df = df_ecmwf_mars %>%
+        rename(mm = "value"), # get in same format as other
+      window_list = list(
+        "primera" = c(5, 6, 7, 8),
+        "postera" = c(9, 10, 11)
+      )
     )
   ),
   tar_target(
-    description= "data.frame (long format) where quantiles defined by RPs 1-10 re provided for each season, country, leadtime",
+    description = "data.frame (long format) where quantiles defined by RPs 1-10 re provided for each season, country, leadtime",
     name = df_mars_q_summary,
-    command = grouped_quantile_summary(df= df_mars_seasonal_summarised ,
-                                       x = "mm",
-                                       rps = c(1:10),
-                                       grp_vars=c("adm0_es","window", "lt")
+    command = grouped_quantile_summary(
+      df = df_mars_seasonal_summarised,
+      x = "mm",
+      rps = c(1:10),
+      grp_vars = c("adm0_es", "window", "lt")
     )
   ),
   # INSUVIMEH
   tar_target(
-    description= "Catalogue of all data received from INSUVIMEH. Useful for troubleshooting at earlier stages when gaps.Gaps have been filled, but still might be useful in future",
+    description = "Catalogue of all data received from INSUVIMEH. Useful for troubleshooting at earlier stages when gaps.Gaps have been filled, but still might be useful in future",
     name = df_gtm_nextgen_catalogue,
-    command = catalogue_insuvimeh_files(gdb = gdb_insuvimeh_gtm,
-                                        file_name_pattern =  "\\d{4}.nc$"),
+    command = catalogue_insuvimeh_files(
+      gdb = gdb_insuvimeh_gtm,
+      file_name_pattern = "\\d{4}.nc$"
+    ),
   ),
-  
+
   # load raster
   tar_target(
-    description = "PackedSpatRaster object containing INSUVIMEH forecast with each publication month leadtime combination as and individual band." ,
+    description = "PackedSpatRaster object containing INSUVIMEH forecast with each publication month leadtime combination as and individual band.",
     name = r_wrap_gtm_nextgen,
-    command = load_insuvimeh_raster(gdb = gdb_insuvimeh_gtm) 
-    
+    command = load_insuvimeh_raster(gdb = gdb_insuvimeh_gtm)
   ),
-  
+
   # run zonal stats on insuvimeh
   tar_target(
     description = "INSUVIMEH monthly zonal means by leadtime",
     name = df_gtm_nextgen_adm0,
-    command= zonal_gtm_insuvimeh(r = unwrap(r_wrap_gtm_nextgen),
-                               gdf = gdf_aoi_adm,
-                               rm_dup_years = F)
+    command = zonal_gtm_insuvimeh(
+      r = unwrap(r_wrap_gtm_nextgen),
+      gdf = gdf_aoi_adm,
+      rm_dup_years = F
+    )
   ),
   # summarise by window, leadtime & pub date
   tar_target(
     description = "INSUVIMEH seasonal forecast sums by leadtime/window",
     name = df_insuvimeh_seasonal_summarised,
-    command = summarise_seasons(df= df_gtm_nextgen_adm0 %>% 
-                                  rename(mm="value") , # get in same format as other
-                                window_list= list("primera"=c(5,6,7,8),
-                                                  "postera"=c(9,10,11))
-                                
+    command = summarise_seasons(
+      df = df_gtm_nextgen_adm0 %>%
+        rename(mm = "value"), # get in same format as other
+      window_list = list(
+        "primera" = c(5, 6, 7, 8),
+        "postera" = c(9, 10, 11)
+      )
     )
   ),
-  
+
   # final decision is to use ECMWF for final month of monitoring to
   # include May forecast in May publication and to not include Sep monitoring of Postrera
   tar_target(
     description = "INSUVIMEH seasonal summaries with May removed from Primera monitoring",
     name = df_insuvimeh_seasonal_summarised_filtered,
-    command =  df_insuvimeh_seasonal_summarised %>% 
+    command = df_insuvimeh_seasonal_summarised %>%
       # used through 2022 for ECMWF - so let's keep it standardized also let's not make threshold
       # harder to hit because of drought that is still largely in effect (2023)
-      filter(year(pub_date)<2023)
+      filter(year(pub_date) < 2023)
   ),
-  
+
   # these will be used for to threshold Guatemala.
   tar_target(
     description = "INSUVIMEH historical record broken into quantile classes by window/lt",
     name = df_insuvimeh_q_summary,
-    command = grouped_quantile_summary(df= df_insuvimeh_seasonal_summarised_filtered,
-                                       x = "mm",
-                                       rps = c(1:10),
-                                       grp_vars=c("adm0_es","window", "lt")
+    command = grouped_quantile_summary(
+      df = df_insuvimeh_seasonal_summarised_filtered,
+      x = "mm",
+      rps = c(1:10),
+      grp_vars = c("adm0_es", "window", "lt")
     )
   ),
-  
-  # final decision was to go w/ RP 4 so saving this for easy manipulation in 
+
+  # final decision was to go w/ RP 4 so saving this for easy manipulation in
   # analysis/threshold_tables.qmd
   tar_target(
     description = "ECMWF & INSUVIMEH Thresholds in long format",
     name = df_all_thresholds_rp4,
     command = bind_rows(
-      df_insuvimeh_q_summary %>% 
-      filter(rp==4) %>% 
-      mutate(
-        forecast_source = "INSUVIMEH"
-      ),
-      df_mars_q_summary %>% 
-        filter(rp ==4) %>% 
+      df_insuvimeh_q_summary %>%
+        filter(rp == 4) %>%
+        mutate(
+          forecast_source = "INSUVIMEH"
+        ),
+      df_mars_q_summary %>%
+        filter(rp == 4) %>%
         mutate(
           forecast_source = "ECMWF MARS"
         )
-  )
+    )
   )
   # will add exploratory modules here.
-  )
-
-
-
+)
