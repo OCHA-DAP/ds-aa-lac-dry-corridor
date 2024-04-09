697586c374d009ccf00a597d2c1efd67a35fda5f
zackarno <zackarno@gmail.com>
2024-04-09 12:52:47 -0400

    run CDs thresholding on zone w/ islands removed to match historical cogs extent

--- data-raw/lac_all_cod_adms.R
+++ data-raw/lac_all_cod_adms.R
@@ -23,3 +23,55 @@ lgdf %>%
       "lac_all_cod_adms.rds"
     )
   )
+
+
+# realize historical CD data is clipped to simplified Shapefile where some of the small caribbean islands are removed
+# therefore to standardize we will remove this from the non-simplifeid version and use that for some zonal stats
+
+
+gdf_adms_full <- read_rds(
+  file.path(
+    Sys.getenv("AA_DATA_DIR"),
+    "public",
+    "processed",
+    "lac",
+    "lac_all_cod_adms.rds"
+  )
+)
+gdf_adm0_full <-  gdf_adms_full$adm0  
+
+gdf_adm0_full_area <- gdf_adm0_full %>% 
+  st_cast("POLYGON") %>% 
+  mutate(
+    area= as.numeric(st_area(.))
+  ) 
+
+gdf_adm0_full_area %>% 
+  st_drop_geometry() %>% 
+  ggplot(
+    aes(x= area)
+  )+
+  geom_histogram()+
+  scale_x_log10(labels=scales::label_comma())
+
+gdf_adm0_poly_no_islands <- gdf_adm0_full_area %>% 
+  filter(
+    area > 1e10
+  ) 
+
+#before
+plot(gdf_adm0_full$geometry)
+
+# after
+gdf_adm0_poly_no_islands$geometry %>% plot()
+write_rds(
+  gdf_adm0_poly_no_islands,
+  file.path(
+    Sys.getenv("AA_DATA_DIR"),
+    "public",
+    "processed",
+    "lac",
+    "lac_cadc_adm0_no_islands.rds"
+  )
+)
+  
