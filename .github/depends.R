9dd5e7e281f6bb611adec99f3ac1d9270ef1b9ac
Zack <zackarno@gmail.com>
2024-06-06 16:58:31 -0400

    update dependencies/yaml to try to move cds monitoring to gha

--- .github/depends.R
+++ .github/depends.R
@@ -4,17 +4,23 @@
 install.packages(c("dplyr",
                    "ggplot2",
                    "readr",
+                   "purrr",
+                   "forcats",
+                   "stringr", # annoying dep just for str_trim to add a single "." to email.
+                   "lubridate",
+                   
                    "sf",
-                   # "rvest", # webscraping
+                   
+                   "googledrive",
+                   
                    "janitor", # data cleaning - used in scraper
                    "terra", # raster manipulation
                    "exactextractr", # zonal stats
                    "tidyr", # pivot_wider
                    # "zoo", # rolling stats
-                   "googledrive",
-                   "purrr",
-                   "lubridate",
-                   "stringr", # annoying dep just for str_trim to add a single "." to email.
+                   
+                   
+                   
                    "here", # set paths in rmds without thinking.
                    "blastula", # rmd emails
                    "glue",  # mainly for pasting text in email.
