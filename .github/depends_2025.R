ec164cb360324ff5ad814827e897b3d9c41496c2
Zack <zackarno@gmail.com>
2025-02-10 13:58:43 -0500

    create req file to attempt to cache reqs on GHA for testing

---
+++ .github/depends_2025.R
@@ -0,0 +1,39 @@
+#' requirements for 2025 monitoring.
+
+# List of required packages
+required_pkgs <- c(
+  "RNetCDF",
+  "terra",
+  "sf", 
+  "dplyr",
+  "exactextractr",
+  "lubridate",
+  "ggplot2", 
+  "ggrepel",
+  "tidyr",
+  "janitor",
+  "stringr",
+  "glue",
+  "rlang", 
+  "purrr",
+  "gghdx",
+  "forcats",
+  "readr",
+  "gt", 
+  "arrow",
+  "geoarrow",
+  "AzureStor", 
+  "ncmeta",
+  "logger",
+  "blastula",
+  "tibble",
+  "showtext",
+  "remotes"
+)
+install.packages(required_pkgs)
+
+# once we have `{remotes}` should be able to install_github
+remotes::install_github(
+  repo = "OCHA-DAP/cumulus"
+  )
+
