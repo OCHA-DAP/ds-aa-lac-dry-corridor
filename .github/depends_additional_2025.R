c915a72a2147dc9c02d3a8bb5a17d0ec081eab0d
Zack <zackarno@gmail.com>
2025-02-10 15:30:31 -0500

    add test_gha_settings.R script to make sure GHA is setting env vars correctly

--- .github/depends_additional_2025.R
+++
@@ -1,5 +0,0 @@
-# ' additional deps
-# i will combine into 1 depends file before moving to main. I just don't 
-# want to wait an hour for the runner to reinstall everything 
-
-install.packages("box")
